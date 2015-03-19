0096825fbf6f90f6b9355884d4ba257ff2525363
Marc Baloup <marc.baloup@laposte.net>
2015-03-19 01:38:55 -0400

    Retrait des tests de la base de donnée

--- application/controleur/commande.php
+++ application/controleur/commande.php
@@ -15,30 +15,13 @@ class Commande extends Controleur{
 	
 	function index($args)
 	{
-		$url_appli = 'index';
 		
 		
-		
-		
+		/*
 		parent::loadModel('Commandes');
-		
-		$cmd = new Commandes(45234, 'sdthbsthstvhsgvhsz',
-		$_POST['largeur'],
-		$_POST['hauteur'],
-		$_POST['marge'],
-		$_POST['epaisseur'],
-	$_POST['bordure_gauche_couleur'],
-	$_POST['bordure_droite_couleur'],
-	$_POST['bordure_haut_couleur'],
-	$_POST['bordure_bas_couleur'],
-	$_POST['bordure_gauche_type'],
-	$_POST['bordure_droite_type'],
-	$_POST['bordure_haut_type'],
-	$_POST['bordure_bas_type']);
-		
-		
+		$cmd = new Commandes(...);
 		$cmd->save();
-		
+		*/
 		
 		require 'application/vue/_template/header.php';
 		require 'application/vue/commande/index.php';
